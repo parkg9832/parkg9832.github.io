6635b993df4c91876db4e5c2658f5643a644cec3
Pablo Gyeom Park <parkg9832@gmail.com>
2026-05-28 09:31:45 +0900

    feat: add editorial brand story layout

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,14 +1,14 @@
 import { AboutContact } from '@/components/about/AboutContact';
 import { AboutFeatures } from '@/components/about/AboutFeatures';
 import { AboutHero } from '@/components/about/AboutHero';
-import { AboutStory } from '@/components/about/AboutStory';
+import { BrandStory } from '@/components/about/BrandStory';
 import { ProductShowcase } from '@/components/product/ProductShowcase';
 
 export default function AboutPage() {
   return (
     <main className="min-h-screen bg-white text-neutral-950">
       <AboutHero />
-      <AboutStory />
+      <BrandStory />
       <ProductShowcase />
       <AboutFeatures />
       <AboutContact />

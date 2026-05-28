b19d86590a70df046f515beb0849316b62d9f733
Pablo Gyeom Park <parkg9832@gmail.com>
2026-05-28 10:18:16 +0900

    feat: update our story copy section

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,6 +2,7 @@ import { AboutContact } from '@/components/about/AboutContact';
 import { AboutFeatures } from '@/components/about/AboutFeatures';
 import { AboutHero } from '@/components/about/AboutHero';
 import { BrandStory } from '@/components/about/BrandStory';
+import { OurStory } from '@/components/about/OurStory';
 import { ProductShowcase } from '@/components/product/ProductShowcase';
 
 export default function AboutPage() {
@@ -9,6 +10,7 @@ export default function AboutPage() {
     <main className="min-h-screen bg-white text-neutral-950">
       <AboutHero />
       <BrandStory />
+      <OurStory />
       <ProductShowcase />
       <AboutFeatures />
       <AboutContact />

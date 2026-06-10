bae471a4cb8726a170826ede5088236a6c3165fd
Pablo Gyeom Park <parkg9832@gmail.com>
2026-06-10 15:07:03 +0900

    Connect B2B form to Apps Script

--- b2b-config.js
+++ b2b-config.js
@@ -1,1 +1,1 @@
-window.MOKDA_B2B_WEB_APP_URL = '';
+window.MOKDA_B2B_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbwGNHXFv2kU2lidTxpoTtNFYf1RPXph4NwU7swo83UqIyyxKcbyDJNtIUXH22Lho_qb/exec';
